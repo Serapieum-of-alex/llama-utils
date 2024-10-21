41dba0d0e073428bfa8cb3e306a319e3e629c2cb
Mostafa Farrag <moah.farag@gmail.com>
2024-10-21 23:14:51 +0200

    add `store` property to the VectorStore class

--- src/llama_utils/retrieval/vector_store.py
+++ src/llama_utils/retrieval/vector_store.py
@@ -7,10 +7,19 @@ from llama_index.core import StorageContext
 class VectorStore:
     """A class to manage vector storage and retrieval."""
 
+    def __init__(self, storage_backend: str = "simple"):
+        # Initialize with the desired vector storage backend (e.g., Qdrant, FAISS)
+        if storage_backend == "simple":
+            self._store = self._create_simple_storage_context()
+
     @staticmethod
-    def _create_simple_storage_context():
+    def _create_simple_storage_context() -> StorageContext:
         return StorageContext.from_defaults(
             docstore=SimpleDocumentStore(),
             vector_store=SimpleVectorStore(),
             index_store=SimpleIndexStore(),
         )
+
+    @property
+    def store(self) -> StorageContext:
+        return self._store
--- tests/retrieval/test_vecrore_store.py
+++ tests/retrieval/test_vecrore_store.py
@@ -1,8 +1,33 @@
 from llama_utils.retrieval.vector_store import VectorStore
+from llama_index.core.storage.docstore import SimpleDocumentStore
+from llama_index.core.storage.index_store import SimpleIndexStore
+from llama_index.core.vector_stores import SimpleVectorStore
+from llama_index.core.graph_stores import SimpleGraphStore
+from llama_index.core import StorageContext
 
 
 def test_create_storage_context():
     # vector_store = VectorStore()
     storage_context = VectorStore._create_simple_storage_context()
-    assert storage_context is not None, "Storage context not created."
+    assert isinstance(storage_context, StorageContext), "Storage context not created."
     assert list(storage_context.vector_stores.keys()) == ["default", "image"]
+    assert (
+        storage_context.vector_stores["default"] is not None
+    ), "Default vector store not created."
+    assert (
+        storage_context.vector_stores["image"] is not None
+    ), "Image vector store not created."
+    assert isinstance(storage_context.docstore, SimpleDocumentStore)
+    assert isinstance(storage_context.index_store, SimpleIndexStore)
+    assert isinstance(storage_context.vector_store, SimpleVectorStore)
+    assert isinstance(storage_context.graph_store, SimpleGraphStore)
+
+
+class TestVectorStore:
+
+    def test_constructor(self):
+        vector_store = VectorStore()
+        assert vector_store is not None, "VectorStore not created."
+        assert (
+            isinstance(vector_store.store, StorageContext) is not None
+        ), "Storage context not created."
