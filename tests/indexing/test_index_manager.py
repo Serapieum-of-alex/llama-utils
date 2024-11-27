b2942df15c5f99ff3b5c1799e1bf709b9e25d528
Mostafa Farrag <moah.farag@gmail.com>
2024-11-27 21:41:23 +0100

    create separate fixture for the paul_graham_essay_storage to fix the test returning value pytest deprecation warning

--- tests/indexing/test_index_manager.py
+++ tests/indexing/test_index_manager.py
@@ -1,3 +1,5 @@
+import pytest
+
 from llama_utils.indexing.custom_index import CustomIndex
 from llama_utils.indexing.index_manager import IndexManager
 from llama_utils.retrieval.storage import Storage
@@ -11,6 +13,18 @@ def test_load_from_empty_storage(storage_path: str):
     assert index_manager.indexes == []
 
 
+def test_load_from_storage_(paul_grahm_essay_storage: str):
+    storage = Storage.load(paul_grahm_essay_storage)
+    index_manager = IndexManager.load_from_storage(storage)
+    assert isinstance(index_manager, IndexManager)
+    assert len(index_manager.indexes) == 2
+    assert index_manager.ids == [
+        "8d57e294-fd17-43c9-9dec-a12aa7ea0751",
+        "edd0d507-9100-4cfb-8002-2267449c6668",
+    ]
+
+
+@pytest.fixture()
 def test_load_from_storage(paul_grahm_essay_storage: str) -> IndexManager:
     storage = Storage.load(paul_grahm_essay_storage)
     index_manager = IndexManager.load_from_storage(storage)
