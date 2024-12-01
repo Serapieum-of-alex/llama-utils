ec8e8208456f0b6d41f825504ba800f0b63cfe9e
Mostafa Farrag <moah.farag@gmail.com>
2024-12-01 22:07:34 +0100

    regroup the class methods in a test class

--- tests/indexing/test_custom_index.py
+++ tests/indexing/test_custom_index.py
@@ -8,37 +8,74 @@ from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_utils.indexing.custom_index import CustomIndex
 
 
-def test_create_from_document(document: Document):
-    content_id = [
-        "8323ac870e04bcf4b64eb04624001a025027d8f797414072df1b81e087f74fb3",
-        "06c83efcd22e3b755ca95ffe22954a304740027769fb541a126a5d23b6f5ac1f",
-    ]
-    doc_list = [document, "any text"]
-    index = CustomIndex.create_from_documents(doc_list)
-    assert isinstance(index, CustomIndex)
-    string = f"\n        Index ID: {index.id}\n        Number of Document: {len(index.doc_ids)}\n        "
-    assert index.__str__() == string
-    assert index.__repr__() == string
-    assert isinstance(index.index, VectorStoreIndex)
-    assert isinstance(index.id, str)
-    assert isinstance(index.vector_store, SimpleVectorStore)
-    assert index.doc_ids == content_id
-    assert isinstance(index.embeddings, dict)
-    assert list(index.embeddings.keys()) == content_id
-    # the embedding is a list of 768 floats based on the BERT model
-    assert len(index.embeddings[content_id[0]]) == 768
+class TestclassMethods:
 
+    def test_create_from_document(self, document: Document):
+        """
+        The test checks if the CustomIndex object is created from a list of a Document and a string objects.
 
-def test_create_from_nodes(text_node: TextNode):
-    index = CustomIndex.create_from_nodes([text_node])
-    assert isinstance(index, CustomIndex)
-    assert len(index.embeddings) == 1
-    assert list(index.index.docstore.docs.keys()) == [text_node.id_]
+        the CustomIndex object should have the following attributes:
+        - id: str
+        - metadata: IndexDict
+            - index metadata data object from llamaindex.core.data_structs.data_structs
+            - The metadata should have a nodes_dict with the node id as the key and the node id as the value.
+            Hint: the node id is not the same as the document id, so the node_dict will have a different key and
+            value, than the doc_ids.
+        - doc_ids: List[str]
+            the value should have two strings in a list, the string in the input is converted into a document and
+            the other document.
+        - embeddings: Dict[str, List[float]]
+            the value should be a dictionary with the node id as the key and the embedding as the value.
+        - vector_store: SimpleVectorStore
+            a Simple vector store object.
+        - index: VectorStoreIndex
+        """
+        content_id = [
+            "8323ac870e04bcf4b64eb04624001a025027d8f797414072df1b81e087f74fb3",
+            "06c83efcd22e3b755ca95ffe22954a304740027769fb541a126a5d23b6f5ac1f",
+        ]
+        doc_list = [document, "any text"]
+        index = CustomIndex.create_from_documents(doc_list)
+        assert isinstance(index, CustomIndex)
+        string = f"\n        Index ID: {index.id}\n        Number of Document: {len(index.doc_ids)}\n        "
+        assert index.__str__() == string
+        assert index.__repr__() == string
+        assert isinstance(index.index, VectorStoreIndex)
+        assert isinstance(index.id, str)
+        assert isinstance(index.vector_store, SimpleVectorStore)
+        assert index.doc_ids == content_id
+        assert isinstance(index.embeddings, dict)
+        assert list(index.embeddings.keys()) == content_id
+        # the embedding is a list of 768 floats based on the BERT model
+        assert len(index.embeddings[content_id[0]]) == 768
 
+    def test_create_from_nodes(self, text_node: TextNode):
+        """
+        The test checks if the CustomIndex object is created from a list of one TextNode object.
 
-def test_create_custome_index_wrong_input():
-    with pytest.raises(ValueError):
-        CustomIndex("wrong input")
+        the CustomIndex object should have the following attributes:
+        - id: str
+        - metadata: IndexDict
+            - index metadata data object from llamaindex.core.data_structs.data_structs
+            - The metadata should habe a nodes_dict with the node id as the key and the node id as the value.
+        - doc_ids: List[str]
+            the value should be an empty list, as the input is a node not a document.
+        - embeddings: Dict[str, List[float]]
+            the value should be a dictionary with the node id as the key and the embedding as the value.
+        - vector_store: SimpleVectorStore
+            a Simple vector store object.
+        - index: VectorStoreIndex
+        """
+        index = CustomIndex.create_from_nodes([text_node])
+        assert isinstance(index, CustomIndex)
+        assert len(index.embeddings) == 1
+        assert list(index.index.docstore.docs.keys()) == [text_node.id_]
+        assert index.doc_ids == []
+        assert index.metadata.nodes_dict == {text_node.id_: text_node.id_}
+
+    def test_create_custome_index_wrong_input(self):
+        with pytest.raises(ValueError):
+            CustomIndex("wrong input")
 
 
 def test_metadata(text_node: TextNode):
